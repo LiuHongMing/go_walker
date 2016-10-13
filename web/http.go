e9cf129ec4ec0802493beddbe5c9e91b158ad82a
liuhongming <liuhongming@senyint.com>
2016-10-13 14:20:03 +0800

    http

--- web/http.go
+++ web/http.go
@@ -4,25 +4,14 @@ import (
 	"net/http"
 	"log"
 	"fmt"
-	"strings"
 )
 
-func sayHelloWorld(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println("form", r.Form)
-	fmt.Println("request", r)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println("host", r.URL.Host)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, " "))
-	}
-	fmt.Fprintf(w, "Hello World!")
+func main() {
+	http.HandleFunc("/hello", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func main() {
-	http.HandleFunc("/hello", sayHelloWorld)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// handler echoes the Path component of the request URL r.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
