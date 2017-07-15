e1309a2c4c5239cb6339c2b9a4090dfeb07ba5fe
J.Ming <jason.liuhongming@gmail.com>
2017-07-15 21:57:58 +0800

    @fix gopher

--- src/gopher/base_type.go
+++ src/gopher/base_type.go
@@ -1,5 +1,6 @@
-/**
+/*
   基本类型
+  类型转换
  */
 
 package main
@@ -9,4 +10,12 @@ import "math"
 func main() {
 	a, b, c, d := 071, 0x1F, 1e9, math.MinInt16
 	println(a, b, c, d)
+
+	var m byte = 100
+	var n int = int(m)
+	println(n)
+
+	if n { // non-bool n (type int) used as if condition
+		println("true")
+	}
 }
