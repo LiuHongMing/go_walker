d3094a10fb179d1c39dc62acf319172ba34b35f5
J.Ming <jason.liuhongming@gmail.com>
2017-07-16 21:57:15 +0800

    @fix gopher

---
+++ src/gopher/pointer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	type data struct {
+		a int
+	}
+
+	var d = data{1234}
+	var p *data
+
+	p = &d
+
+	fmt.Printf("%p, %v\n", p, p.a)
+
+	x := 0x12345679
+	q := unsafe.Pointer(&x)
+	n := (*[4]byte)(q)
+
+	for i := 0; i < len(n); i++ {
+		fmt.Printf("%X ", n[i])
+	}
+
+	y := struct {
+		s string
+		x int
+	}{"abc", 100}
+
+	w := uintptr(unsafe.Pointer(&y))
+	w += unsafe.Offsetof(y.x)
+
+	w2 := unsafe.Pointer(w)
+	px := (*int) (w2)
+	*px = 200
+
+	fmt.Printf("%#v\n", y)
+}
+
+func test3() *int {
+	x := 100
+	return &x
+}
--- src/gopher/strings.go
+++ src/gopher/strings.go
@@ -1,1 +1,51 @@
 package main
+
+import "fmt"
+
+func main() {
+	s := "abc"
+	println(s[0] == '\x61', s[1] == 'b', s[2] == '\x63')
+
+	s = `a
+	b\r\n\x00
+	c
+	`
+	println(s, &s)
+
+	s = "Hello, " +
+		"World !"
+	println(s)
+
+	s1 := s[:5]
+	s2 := s[7:]
+	s3 := s[1:5]
+	println(s1, s2, s3)
+
+	fmt.Printf("%T\n", 'a')
+	var c1, c2 rune = '\u6211', '们'
+	println(c1 == '我', string(c2) == "\xe4\xbb\xac")
+
+	// byte
+	s = "abcd"
+	bs := []byte(s)
+	bs[1] = 'B'
+	println(string(bs))
+
+	// rune
+	u := "电脑"
+	rs := []rune(u)
+	rs[1] = '话'
+	println(string(u))
+
+	s = "abc汉字"
+
+	// byte
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%c,", s[i])
+	}
+	fmt.Printf("\n")
+	//rune
+	for _, r := range s {
+		fmt.Printf("%c,", r)
+	}
+}
